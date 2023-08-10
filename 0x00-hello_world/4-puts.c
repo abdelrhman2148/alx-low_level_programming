94d8e3d1f2aa843bee20700771cb9056bfc53a76
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-10 00:10:17 -0700

    print without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * Main - Entry Point
  *
@@ -11,5 +10,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
