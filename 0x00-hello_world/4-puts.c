655d518f4e1d12e2fb024d0d52e7815213adb510
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-09 23:55:36 -0700

    print without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
+/* this function print the words inside puts */
+
+int main(void)  {
 	    puts("\"Programming is like building a multilingual puzzle");
 	    return 0;
-}
-
+		}
