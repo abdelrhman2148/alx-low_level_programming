4cf5bc8774c371830de6fe68f25363b19f046a14
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-10 00:42:28 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C prog that prints with printf
+ *
+ * Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
