8a3ffc39d66951e4717d42c9c64eff329f040190
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-10 00:05:12 -0700

    print without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-/* this function print the words inside puts */
+/**
+ * Main - Entry Point
+ *
+ * Description: A C prog that prints wihout printf.
+ *
+ * Return: Always 0 (Succes)
+ */
 
-int main(void)  {
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return 0;
-		}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
