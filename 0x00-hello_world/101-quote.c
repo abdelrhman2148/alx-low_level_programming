6ebd249a26fce2f8fde87769372019025d27cf70
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-10 19:50:03 -0700

    printing in an advanced way

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+i#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this C progrsm prints without puts and printf can u imagine?
+ *
+ * Return: 1 (not succes) 
+*/
+
+int main(void)
+{
+	char qoute[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.qoute.59);
+	return (1);
+}
