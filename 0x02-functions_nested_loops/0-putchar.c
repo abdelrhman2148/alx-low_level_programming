9c9911bb8910d307675b2a3e9590724a23b3e0c2
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-14 21:12:47 -0700

    this commit for putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: this c program print _putchar as string
+ *
+ * Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This read me file is to show my nested loop and C function Task
