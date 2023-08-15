88d197d1b2fff03910986ed27732b10110054d4e
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 11:00:53 -0700

    this function times table with n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,10 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
-			for (multi = 1; multi <= n; ++multi)
+			for (multi = 1; multi <= n; multi++)
 			{
 				_putchar(',');
 				_putchar(' ');
