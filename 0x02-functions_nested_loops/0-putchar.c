01f141a3c87526a188e492fdf084f8f6b8032a0f
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 07:51:31 -0700

    this commit for add 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
- * Description: this c program print _putchar as string
+ * Description: this c program print _putchar as prototype
  *
  * Return: Always 0 (sucess)
 */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int character;
+
+	for (character = 0; character < 8; character++)
+		_putchar(str[character]);
+	_putchar('\n');
 
 	return (0);
 }
