d1581abcc3c1b7120c465e9fbbd72a89225376a0
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 08:27:56 -0700

    this commit for checks is a;pha?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - this function checks if character aletter
+ *
+ * @c: this parameter takes input from the other function
+ *
+ * Return: 1 if c is true else 0 Always (succes)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
