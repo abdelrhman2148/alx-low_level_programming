02f640931871e4815c6e0915a2ab171d76853115
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 09:52:26 -0700

    this for an add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this function add two num
+ *
+ * @num1: the first int
+ * @num2: the sec int num
+ *
+ * Return: sum of the num
+*/
+int add(int num1, num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
