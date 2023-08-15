b79f110cccab45b4d6f0f77be1155d11468bcc66
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 10:56:56 -0700

    this function times table with n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_times_table - this function print time tables with n
+ *
+ * @n: this prametert work as input that takes integer
+ *
+*/
+
+void print_times_table(int n)
+{
+	int prod, num, multi;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (multi = 1; multi <= n; ++multi)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * multi;
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 99)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
