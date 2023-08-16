bce2f9e194ce440920a445f4256567ba6117e6e0
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 18:00:25 -0700

    Fibonacci numbers final version

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: C prog for fibonacci numbers last version
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int a = 1, b = 2, count = 0;
+
+	printf("%d, %d, ", a, b);
+	count += 2;
+
+	while (count < 98)
+	{
+		int next = a + b;
+
+		if (count == 97)
+		{
+			printf("%d\n", next);
+		}
+		else
+		{
+			printf("%d, ", next);
+		}
+		count++
+
+		a = b;
+		b = next;
+	}
+	return (0);
+}
+
