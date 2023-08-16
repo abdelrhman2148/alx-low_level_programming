02810cd16b7ac2cb8e1fdc2216356b601ec105c9
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-15 17:37:43 -0700

    Fibonacci numbers v-2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: this C program simulate foboncci numbers v-2
+ *
+ * Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	unsigned long num1 = 0, num2 = 0, sum;
+	float total;
+
+	while (1)
+	{
+		sum = num1 + num2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total += sum;
+		num1 = num2;
+		num2 = sum;
+
+	}
+	printf("%.0f\n", total);
+	return (0);
+}
