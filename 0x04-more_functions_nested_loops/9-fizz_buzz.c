3047bb8771c70195ed1a14d8bc3056eeb5ddd40a
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-17 02:41:42 -0700

    11

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,27 @@
 int main(void)
 {
 	int i;
-
-
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (x != 100)
+		{
+		printf(" ");
 		}
 	}
 	printf("\n");
