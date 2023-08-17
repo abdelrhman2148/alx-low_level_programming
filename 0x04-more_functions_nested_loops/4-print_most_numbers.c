a6c818a40b095cc339f6e600dea37c86edcb4a73
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-17 02:22:52 -0700

    05

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - this function prints with
- * 		putchar from 0 to 9
- * 		except 2 and 4
+ * print_most_numbers - this function prints with putchar from 0 to 9
  *
  * Return: Always 0 (sucess)
 */
