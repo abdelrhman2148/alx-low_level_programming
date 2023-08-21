ee40b80f1260545f5a97ff82d7ef5a9725dfb3c3
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-21 05:15:09 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Print the second half of a string followed by a newline.
+ * @str: The input string.
+*/
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+	int i;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length + 1) / 2;
+	}
+	for (i = start_index; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
