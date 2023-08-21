f1fd93e4399f947c8017a156ab0efeb95877826b
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-21 07:48:32 -0700

    01

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ *
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+*/
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+	return (result * sign);
+}
