20e5ff85fd9f2a784c8b70fd61c2c16c350bceaf
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-23 11:09:40 -0700

    captilaize the strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,46 @@
-#include <ctype.h>
+#include <stdbool.h>
 #include "main.h"
 /**
- * *cap_string - this function is to capitalize all characters
+ * is_separator - this function is to deal with saparators
+ *
+ * @c: the input char
+ *
+ * Return: true or false
+*/
+bool is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (true);
+		}
+	}
+	return (false);
+}
+
+/**
+ * *cap_string - this function is to capitalize all characte
  *
  * @str: the input string
  *
  * Return: str as string
 */
-
 char *cap_string(char *str)
 {
-	int i, capitalize_next = 1;
+	bool new_word = true;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (isspace((unsigned char)str[i]) || ispunct((unsigned char)str[i]))
+		if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
 		{
-			capitalize_next = 1;
+			str[i] = str[i] - 'a' + 'A';
 		}
-		else if (capitalize_next && isalpha((unsigned char)str[i]))
-		{
-			str[i] = toupper((unsigned char)str[i]);
-			capitalize_next = 0;
-		}
-		else
-		{
-			str[i] = tolower((unsigned char)str[i]);
-		}
+		new_word = is_separator(str[i]);
 	}
 	return (str);
 }
