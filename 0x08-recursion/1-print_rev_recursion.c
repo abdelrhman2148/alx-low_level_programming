d0bae95972e1f4420fb7f14741ed3da1675e090b
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-08-30 07:18:54 -0700

    rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - this function print strings in reverse
+ *
+ * @s: the input string
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,5 @@
 
 int _putchar(char ch);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
