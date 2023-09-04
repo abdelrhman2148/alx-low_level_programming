550bf66f74af063ce6f2419592fbddcdbb86aaf7
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-04 10:29:20 -0700

    grid alloc function 04

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,16 +17,15 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
-	
 	}
-	grid = (int **)calloc(height, sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)calloc(width, sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -35,6 +34,13 @@ int **alloc_grid(int width, int height)
 			}
 			free(grid);
 			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
 		}
 	}
 	return (grid);
