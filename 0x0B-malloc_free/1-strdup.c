167626049d11d1c36ef96087fad491ec9aec360f
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-04 08:31:11 -0700

    custome strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_strdup - this function takes a string str as a parameter and returns a
+ *                    pointer to a newly allocated space in memory containing
+ *                          a copy of the input string
+ *
+ * @str: the input string
+ *
+ * Return: duplicate.
+*/
+char *_strdup(char *str)
+{
+	size_t length = strlen(str);
+	char *duplicate = (char *)malloc((length + 1) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
