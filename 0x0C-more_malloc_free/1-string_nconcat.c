c3f4f707bf89fcfac5744edb3b48b9873201daac
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-07 09:00:25 -0700

    two stings to concat 03

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-	result = (char *)malloc(len1 + n + 1);
+	result = malloc(len1 + n + 1);
 	if (result == NULL)
 	{
 		return (NULL);
