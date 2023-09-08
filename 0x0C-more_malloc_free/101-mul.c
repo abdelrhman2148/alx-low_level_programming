44aef72f6d9f48a75676e91e564d38aec71a4cd6
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-07 17:13:11 -0700

    muultiplyer program full edithion v1.12

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,60 +2,56 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * isNumeric - this function check if a string contains only digits
+ * isDigitString - Checks if a string contains only digits.
  *
- * @str: this is the string will check
+ * @str: The string to be checked.
  *
- * Return: 0 or 1
+ * Return: 1 if all characters are digits, 0 otherwise.
 */
-int isNumeric(const char *str)
+int isDigitString(char *str)
 {
-	while (*str)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (!isdigit(*str))
+		if (!isdigit(str[i]))
+		{
 			return (0);
-		str++;
+		}
 	}
 	return (1);
 }
 /**
- * multiply - this function multiply two positive numbers
+ * main - Entry point of the program.
  *
- * @num1: first int taken
- * @num2: secound int taken
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
  *
- * Return: multi of the two int
-*/
-long multiply(int num1, int num2)
-{
-	return ((long)num1 * num2);
-}
-/**
- * main - Entry point
- *
- * @argc: int
- * @argv: pointer to char
- *
- * Return: 98 or 0
+ * Return: 0 on success, 98 on error.
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	long result;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
+	if (!isDigitString(argv[1]) || !isDigitString(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	result = multiply(num1, num2);
-	printf("%ld\n", result);
+
+	if (num1 < 0 || num2 < 0)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	result = num1 * num2;
+	printf("%d\n", result);
 	return (0);
 }
