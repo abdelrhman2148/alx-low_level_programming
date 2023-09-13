6bb752a3a569e0329e570ddbe5291d6913c22811
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-12 22:25:35 -0700

    calc v2.05

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - Entry point
+ *
+ * @argc: count arg
+ * @argv: vector arg
+ *
+ * Return: 99 or 98 or 0.
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*op_func)(int, int);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	result = op_func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
