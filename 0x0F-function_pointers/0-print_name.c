f6c91384469c89f85de52108fe47ea5f51374175
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-12 21:05:34 -0700

    055

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_name - this function takes a name as a string and a function pointer
+ *                          to print the name either as is or in uppercase
+ *
+ * @name: the name
+ * @f: a function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this readme file for the task 0x0F-function_pointers.
