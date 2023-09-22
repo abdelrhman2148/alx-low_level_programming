847976f3aa0ac707a651470d048b16ef7f216e7d
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-22 06:31:33 -0700

    ass

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    hello db "Hello, Holberton",0
+
+section .text
+    global main
+
+    extern printf
+
+main:
+    push rbp
+    mov rdi, hello
+    call printf
+    add rsp, 8  ; Clean up the stack
+    pop rbp
+
+    ; Exit the program
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; status: 0
+    syscall
