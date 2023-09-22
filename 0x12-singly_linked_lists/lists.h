67e1a5f9ce4003211dbd8cd2dd50d6be52eb5caf
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-22 05:46:09 -0700

    a1

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - Singly linked list structure
+ * @str: String data stored in the node
+ * @len: Length of the string
+ * @next: Pointer to the next node in the list
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
