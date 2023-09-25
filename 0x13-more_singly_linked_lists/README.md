9bb60357b74a77fe329466ebc847cde6c122439a
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-25 16:05:26 -0700

    af task0

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This readme file for describing more linked list task.
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * struct listint_s - A structure representing a singly linked list node.
+ * @n: An integer value stored in the node.
+ * @next: A pointer to the next node in the list.
+ *
+ * Description: This structure defines a node in a singly linked list,
+ * where 'n' contains the integer data and 'next' points to the next node.
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
