387ea50ae38ebcaadb58147185d27703ed223c57
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-25 16:13:03 -0700

    af task2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - A structure representing a singly linked list node.
  * @n: An integer value stored in the node.
@@ -19,5 +20,6 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
